6e7b71b5729a69b65bf473d1d8f3a8ae230d2166
hashemh <hashemh@umich.edu>
2018-02-23 10:49:09 -0500

    Added tests for chained subtract, multiplication, and division

--- test_rpn.py
+++ test_rpn.py
@@ -13,6 +13,31 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('5 2 -')
         self.assertEqual(3, result)
 
+    def test_subtracts(self):
+        result = rpn.calculate('5 2 - 2 -')
+        self.assertEqual(1, result)
+
+    def test_addAndSubtract(self):
+        result = rpn.calculate('1 1 + 2 + 3 - 2 + 4 -')
+        self.assertEqual(-1, result)
+
+    def test_multiply(self):
+        result = rpn.calculate('3 5 *')
+        self.assertEqual(15, result)
+
+    def test_multiplys(self):
+        result = rpn.calculate('3 5 * 2 *')
+        self.assertEqual(30, result)
+
+    def test_divide(self):
+        result = rpn.calculate('3 5 /')
+        self.assertEqual((3/5), result)
+
+    def test_divides(self):
+        result = rpn.calculate('5 10 / 2 /')
+        self.assertEqual(1/4, result)
+
     def test_tooMany(self):
         with self.assertRaises(TypeError):
             result = rpn.calculate('1 2 3 +')
+
