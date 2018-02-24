ce23579e736e95247f61ce91e928d66b5603e387
hashemh <hashemh@umich.edu>
2018-02-24 15:36:24 -0500

    Added printHistory function

--- rpn.py
+++ rpn.py
@@ -15,16 +15,35 @@ def factorial(base):
         return 1
     return base * factorial(base-1)
 
+def printHistory(history):
+    if len(history) == 3:
+        answer = history.pop()
+        token = history.pop()
+        arg = history.pop()
+        return str(arg) + " " + str(token) + \
+                " = " + str(answer)
+    elif len(history) == 4:
+        answer = history.pop()
+        token = history.pop()
+        arg2 = history.pop()
+        arg1 = history.pop()
+        return str(arg1) + " " + str(token) + " " + str(arg2) \
+                + " = " + str(answer)
+    else:
+        raise TypeError
+
 operators = {
     '+': operator.add,
     '-': operator.sub,
     '*': operator.mul,
     '/': operator.truediv,
     '!': factorial,
+    'history()': printHistory,
 }
 
 def calculate(arg):
     stack = list()
+    history = list()
     for token in arg.split():
         try:
             value = float(token)
@@ -34,11 +53,19 @@ def calculate(arg):
             result = 0
             if function == factorial:
                 arg = stack.pop()
+                history.append(arg)
+                history.append(token)
                 result = function(arg)
+            elif function == printHistory:
+                return printHistory(history)
             else:
                 arg2 = stack.pop()
                 arg1 = stack.pop()
+                history.append(arg1)
+                history.append(arg2)
+                history.append(token)
                 result = function(arg1, arg2) 
+            history.append(result)
             stack.append(result)
         logger.debug(stack)
 
