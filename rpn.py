a9ff55f1784793f8f99179d02e7d6a8e9befc269
hashemh <hashemh@umich.edu>
2018-02-24 20:07:06 -0500

    Fixed minor problem in history

--- rpn.py
+++ rpn.py
@@ -8,6 +8,8 @@ logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stdout)
 logger.addHandler(ch)
 
+history = list()
+
 def factorial(base):
     if base < 0:
         raise ArithmeticError
@@ -15,22 +17,29 @@ def factorial(base):
         return 1
     return base * factorial(base-1)
 
-def printHistory(history):
+def printHistory():
+    global history
+    output = None
     if len(history) == 3:
         answer = history.pop()
         token = history.pop()
         arg = history.pop()
-        return str(arg) + " " + str(token) + \
-                " = " + str(answer)
+        output = str(arg) + " " + str(token) + \
+                 " = " + str(answer)
     elif len(history) == 4:
         answer = history.pop()
         token = history.pop()
         arg2 = history.pop()
         arg1 = history.pop()
-        return str(arg1) + " " + str(token) + " " + str(arg2) \
-                + " = " + str(answer)
-    else:
+        output = str(arg1) + " " + str(token) + \
+                 " " + str(arg2) + " = " + str(answer)
+
+    if (len(history) > 0):
         raise TypeError
+    if (output is None):
+        output = "No history is present"
+    return output
+
 
 operators = {
     '+': operator.add,
@@ -42,8 +51,8 @@ operators = {
 }
 
 def calculate(arg):
+    global history
     stack = list()
-    history = list()
     for token in arg.split():
         try:
             value = float(token)
@@ -52,13 +61,15 @@ def calculate(arg):
             function = operators[token]
             result = 0
             if function == factorial:
+                history = list()
                 arg = stack.pop()
                 history.append(arg)
                 history.append(token)
                 result = function(arg)
             elif function == printHistory:
-                return printHistory(history)
+                return printHistory()
             else:
+                history = list()
                 arg2 = stack.pop()
                 arg1 = stack.pop()
                 history.append(arg1)
